cd53b870abe03d75fc4757ee37d3a099b107abdf
Sushil <grayson.9999@gmail.com>
2025-06-27 07:58:57 +1000

    starting backend for Library Mangament System

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+import cors from "cors";
+import morgan from "morgan";
+// middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// server status
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is live!",
+  });
+});
+
+app.listen(PORT, (error) =>
+  error
+    ? console.log(error)
+    : console.log(`server is running at http://localhost:${PORT}`)
+);
